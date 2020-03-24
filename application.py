7bba559f8b53c2e105e8c537fe6e1899ac84ab3e
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-03-24 14:39:57 +0200

    create function to display name

--- application.py
+++ application.py
@@ -1,13 +1,24 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+channels = []
 
-@app.route("/")
+@app.route("/", methods = ["GET"])
+def render_index():
+        return render_template("index.html")
+
+
+@app.route("/", methods = ["POST"])
 def index():
-    return "Project 2: TODO"
+        return render_template("index.html")
+
+
+@app.route("/add_channel", methods=["GET"])
+def render_add_channel():
+    
