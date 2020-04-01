96c8aca47e6906c47ab830e696df181e465bb166
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-04-01 11:34:33 +0300

    add new user socket

--- application.py
+++ application.py
@@ -1,27 +1,24 @@
 import os
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-channels = ["cooking", "gardening", "cleaning", "clothes"]
+
+# @app.route("/", methods = ["GET"])
+# def get_username():
+#         return render_template("login.html")
 
 @app.route("/", methods = ["GET"])
 def render_index():
         return render_template("index.html")
 
-
 @app.route("/", methods = ["POST"])
 def index():
         return render_template("index.html")
-
-# @app.route("/cooking", methods=["GET"])
-# def render_cooking():
-#     return render_template("index.html", channels=channels)
-
 
 @socketio.on("submit message")
 def message(data):
@@ -36,3 +33,7 @@ def delete_message(data):
         message["time"] = data["message"]["time"] 
         message["id"] = data["message"]["id"] 
         emit("display deleted message", {"message": message}, broadcast=True)
+
+@socketio.on("new user")
+def display_username(username):
+        emit("add user", {"username":username}, broadcast=True)
